e3fa8cf4a734116d3e140eb1bd8ae9343e2c82e5
yanyanyaa <yclin3411@gmail.com>
2024-05-15 01:49:22 +0800

    feat: add relations between user & todo models

--- models/todo.js
+++ models/todo.js
@@ -5,7 +5,7 @@ module.exports = (sequelize, DataTypes) => {
     isDone: DataTypes.BOOLEAN
   }, {});
   Todo.associate = function(models) {
-    // associations can be defined here
+    Todo.belongsTo(models.User)
   };
   return Todo;
 };
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const router = express.Router()
-
-// 載入資料
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
 // 路由設定
 router.get('/register', (req, res) => {
   res.render('register')
 })
 
-// router.post('/register', (req, res) => {
-// })
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+})
 
 router.get('/login', (req, res) => {
   res.render('login')
