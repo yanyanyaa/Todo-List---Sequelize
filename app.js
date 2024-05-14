a3a519166bac56df5672ee58228e3352a4266b7e
yanyanyaa <yclin3411@gmail.com>
2024-05-15 00:17:09 +0800

    project init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const app = express()
+const PORT = process.env.PORT
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname:'.hbs' }))
+app.set('view engin', 'hbs')
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(PORT, () => {
+  console.log(`app is running on http://localhost:${PORT}`)
+})
